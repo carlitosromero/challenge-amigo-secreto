872af28ecddd9b6273aecf2b1b60b1f2ed1dc6f5
Carlos Romero <romerohuamancarlosdaniel@gmail.com>
2025-03-18 22:32:08 -0500

    refactor: mejorar la legibilidad del código eliminando puntos y comas innecesarios

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-let amigos = [];
+let amigos = []
 
 /**
  * Agrega un nuevo amigo al array de amigos.
  * Valida que el nombre no esté vacío antes de agregarlo y actualiza la lista visual.
  */
 function agregarAmigo() {
-  let nombreAmigo = document.getElementById("amigo").value;
+  let nombreAmigo = document.getElementById('amigo').value
 
-  if (nombreAmigo.trim() === "") {
-    alert("Por favor, inserte un nombre");
+  if (nombreAmigo.trim() === '') {
+    alert('Por favor, inserte un nombre')
   } else {
-    amigos.push(nombreAmigo);
-    document.querySelector("#amigo").value = "";
-    mostrarListaAmigo();
+    amigos.push(nombreAmigo)
+    document.querySelector('#amigo').value = ''
+    mostrarListaAmigo()
   }
 }
 
@@ -20,15 +20,15 @@ function agregarAmigo() {
  * Actualiza la visualización de la lista de amigos en el DOM, creando elementos <li> para cada amigo.
  */
 function mostrarListaAmigo() {
-  let listaAmigos = document.querySelector("#listaAmigos");
-  listaAmigos.innerHTML = "";
+  let listaAmigos = document.querySelector('#listaAmigos')
+  listaAmigos.innerHTML = ''
 
   for (let index = 0; index < amigos.length; index++) {
-    const element = amigos[index];
+    const element = amigos[index]
 
-    let listaHTML = document.createElement("li");
-    listaHTML.textContent = element;
-    listaAmigos.appendChild(listaHTML);
+    let listaHTML = document.createElement('li')
+    listaHTML.textContent = element
+    listaAmigos.appendChild(listaHTML)
   }
 }
 
@@ -37,12 +37,12 @@ function mostrarListaAmigo() {
  * Verifica que la lista no esté vacía antes de realizar el sorteo.
  */
 function sortearAmigo() {
-  let cantidadAmigos = amigos.length;
+  let cantidadAmigos = amigos.length
   if (cantidadAmigos === 0) {
-    alert("Por favor, inserte un nombre antes de sortear");
+    alert('Por favor, inserte un nombre antes de sortear')
   } else {
-    let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
-    let resultadoHTML = document.querySelector("#resultado");
-    resultadoHTML.innerHTML = amigos[indiceAmigo];
+    let indiceAmigo = Math.floor(Math.random() * cantidadAmigos)
+    let resultadoHTML = document.querySelector('#resultado')
+    resultadoHTML.innerHTML = amigos[indiceAmigo]
   }
 }
